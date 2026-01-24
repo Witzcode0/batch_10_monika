7d28cc04c7d250d324975b80e25469f865ff07c5
Witzcode0 <brijesh.gondaliya07@gmail.com>
2026-01-24 15:50:58 +0530

    control-statement and list half

---
+++ core-python/control-statements/main.py
@@ -0,0 +1,11 @@
+for num in range(1, 11):
+    if num == 2:
+        continue
+
+    if num == 5:
+        pass
+
+    if num == 7:
+        break
+    
+    print(num)
---
+++ core-python/data-collections/1-list.py
@@ -0,0 +1,66 @@
+"""
+Docstring for core-python.data-collections.4-dict
+
+List | mutable | ordered | indexing | slicing | dupllicate values are allowed
+
+syntax:
+
+list_name = list()
+list_name = []
+
+Example :
+
+list_name = [ele1, ele2, ..., elen]
+"""
+
+nums = []
+
+# print(type(nums))
+# print(len(nums))
+
+nums = [1,2,3,4,5]
+
+# print(nums)
+# print(len(nums))
+
+# print(nums)
+
+# indexing(+/-)
+# print(nums[0])
+# print(nums[1])
+# print(nums[2])
+# print(nums[3])
+# print(nums[4])
+
+# print(nums[-3])
+# print(nums[-4])
+
+# fruits = ["apple", "banana", "guava", "mango", "watermelon"]
+
+# print(fruits[1])
+# print(fruits[-4])
+
+# fruits[1] = "pinapple"
+
+# print(fruits)
+
+# slicing(+/-)
+# obj_name[start : end + 1 : step + 1]
+# print(fruits[1 : 4 ])
+# print(fruits[2:])
+# print(fruits[:3])
+
+
+# obj_name[start : end - 1 : step - 1]
+# print(fruits[::-1])
+# print(fruits[-1:-4:-1])
+
+
+# nums = list(range(1, 11))
+# print(nums)
+# print(nums[::1])
+# # print(nums[::2])
+# print(nums[::3])
+
+# nums = [1,1,2,43,1,4,1,3,1,3,54,76,87,45,34,56,76,8,10]
+# print(nums)
